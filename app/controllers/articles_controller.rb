193282c3f2eabe2d874ccbcf31b0cb9eb18ca06b
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-19 19:08:04 +0100

    Adds dynamic sources array depending on selected language Adds randomizer to output 2 random sources with all links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,17 +4,28 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
-
-    newsapi = News.new(Rails.application.credentials.news_api[:secret_key])
-
-    @sources = ["bbc-news","buzzfeed"]
-
-    @all_articles = newsapi.get_top_headlines(sources: @sources.join(', '))
+    @newsapi = News.new(Rails.application.credentials.news_api[:secret_key])
+    @sources = get_and_set_sources
+    @all_articles = @newsapi.get_top_headlines(sources: @sources.join(', '))
   end
 
   def show
     @article = Article.find(params[:id])
   end
 
-end
+  private
 
+  def get_and_set_sources
+    sourcesArray = []
+    returnArray = []
+    source = params[:locals][:source]
+    sources = @newsapi.get_sources(language: source)
+    sources.each do |source|
+      sourcesArray.push(source.id)
+    end
+    2.times do
+      returnArray.push(sourcesArray[rand(sourcesArray.length)])
+    end
+    return returnArray
+  end
+end
