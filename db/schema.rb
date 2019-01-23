fa624c37c32f55b8405affe49015e2d19e877fe2
Aditya Naik <naik.aditya@gmail.com>
2019-01-23 19:16:45 +0100

    Updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2019_01_18_105446) do
   end
 
   create_table "comments", force: :cascade do |t|
+    t.string "commenter"
     t.text "body"
     t.bigint "article_id"
     t.datetime "created_at", null: false
