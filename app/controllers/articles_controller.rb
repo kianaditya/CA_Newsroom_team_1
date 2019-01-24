e6ea70140bfc22a5e2417819f90a03cdea6d719e
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-24 21:41:52 +0100

    Refactors category show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,16 @@
 class ArticlesController < ApplicationController
 
   def index
-    @index_article = Article.first
-    @secondary_articles = Article.all[1..2]
-    @articles = Article.all[3..-1]
-
+    if params[:category].present?
+      article = Article.where(category_id: params[:category])
+      @index_article = article.first
+      @secondary_articles = article.all[1..2]
+      @articles = article.all[3..-1]
+    else
+      @index_article = Article.first
+      @secondary_articles = Article.all[1..2]
+      @articles = Article.all[3..-1]
+    end
   end
 
   def show
