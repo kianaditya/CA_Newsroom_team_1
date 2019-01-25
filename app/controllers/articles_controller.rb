5d5d87c7e04ac8ed0ec74b14d62cc1910c7ab46a
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-25 08:46:50 +0100

    Styles the localization

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,11 +5,11 @@ class ArticlesController < ApplicationController
       article = Article.where(category_id: params[:category])
       @index_article = article.first
       @secondary_articles = article.all[1..2]
-      @articles = article.all[3..-1]
+      @articles = article.all[3..99]
     else
       @index_article = Article.first
       @secondary_articles = Article.all[1..2]
-      @articles = Article.all[3..-1]
+      @articles = Article.all[3..99]
     end
   end
 
