9b0618d6318694b359d8a4bd60280eb489e6626f
hanna_nyman <hannanyman.sk@gmail.com>
2019-01-25 10:42:32 +0100

    [WIP] Fixes tests going red after re-styling root page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,6 @@ class ApplicationController < ActionController::Base
   
   def after_sign_in_path_for(resource)
     if resource.admin?
-      binding.pry
       cms_articles_path
     else
       super
